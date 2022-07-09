48749fe10b32a21404badac75367f2018f729ccb
Lisa Heffernan <lisaheffernan@outlook.com>
2022-07-09 21:50:13 +0100

    refactored tests so if variables in js file change the tests won't fail

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,4 +1,23 @@
-const Pet = require("../src/pet");
+const {
+  minFitness,
+  maxFitness,
+  minHunger,
+  maxHunger,
+  maxAge,
+  fitnessBreakPoint,
+  hungerBreakPoint,
+  ageIncrement,
+  hungerIncrement,
+  fitnessReduction,
+  feedReduction,
+  exerciseIncrement,
+  petDeadMessage,
+  hungryAndWalkMessage,
+  walkMessage,
+  hungryMessage,
+  allHappyMessage,
+  Pet,
+} = require("../src/pet");
 
 describe("constructor", () => {
   it("returns an object", () => {
@@ -13,27 +32,27 @@ describe("constructor", () => {
     expect(pet2.name).toEqual("Rex");
   });
 
-  it("has a initial age of 0", () => {
+  it(`has a initial age of 0`, () => {
     const pet = new Pet("Fido");
 
     expect(pet.age).toEqual(0);
   });
 
-  it("has a initial hunger of 0", () => {
+  it(`as a initial hunger of ${minHunger}`, () => {
     const pet = new Pet("Fido");
 
-    expect(pet.hunger).toEqual(0);
+    expect(pet.hunger).toEqual(minHunger);
   });
 
-  it("has a initial fitness of 10", () => {
+  it(`has a initial fitness of ${maxFitness}`, () => {
     const pet = new Pet("Fido");
 
-    expect(pet.fitness).toEqual(10);
+    expect(pet.fitness).toEqual(maxFitness);
   });
 
   it("has children array initially with no elements", () => {
     const pet = new Pet("Fido");
-    
+
     expect(pet.children.length).toEqual(0);
   });
 });
@@ -45,32 +64,32 @@ describe("growUp", () => {
     expect(pet.growUp).toBeInstanceOf(Function);
   });
 
-  xit("increments the age by 1", () => {
+  it(`increments the age by ${ageIncrement}`, () => {
     const pet = new Pet("Fido");
     pet.growUp();
 
-    expect(pet.age).toEqual(1);
+    expect(pet.age).toEqual(0 + ageIncrement);
   });
 
-  xit("increments the hunger by 5", () => {
+  it(`increments the hunger by ${hungerIncrement}`, () => {
     const pet = new Pet("Fido");
     pet.growUp();
 
-    expect(pet.hunger).toEqual(5);
+    expect(pet.hunger).toEqual(minHunger + hungerIncrement);
   });
 
-  xit("decreases the fitness by 3", () => {
+  it(`decreases the fitness by ${fitnessReduction}`, () => {
     const pet = new Pet("Fido");
     pet.growUp();
 
-    expect(pet.fitness).toEqual(7);
+    expect(pet.fitness).toEqual(maxFitness - fitnessReduction);
   });
 
   it("throws an error if the pet is not alive", () => {
     const pet = new Pet("Fido");
-    pet.age = 30;
+    pet.age = maxAge;
 
-    expect(() => pet.growUp()).toThrow("Your pet is no longer alive :(");
+    expect(() => pet.growUp()).toThrow(petDeadMessage);
   });
 });
 
@@ -81,28 +100,28 @@ describe("walk", () => {
     expect(pet.walk).toBeInstanceOf(Function);
   });
 
-  xit("increases fitness by 4", () => {
+  it(`increases fitness by ${exerciseIncrement}`, () => {
     const pet = new Pet("fido");
-    pet.fitness = 4;
+    pet.fitness = maxFitness - 2 * fitnessReduction;
     pet.walk();
 
-    expect(pet.fitness).toEqual(8);
+    expect(pet.fitness).toEqual(
+      maxFitness - 2 * fitnessReduction + exerciseIncrement
+    );
   });
-  // How do we know what the max value for fitness is, or any other value we are testing for? Would it be a good idea to store these values in variables in this file as it is in pet.js? Otherwise whenever a change is made to the main file, you would have to go through the test file and check every individual test to see if any have been missed.
-  xit("make sure fitness does not go above max fitness", () => {
+
+  it(`make sure fitness does not go above ${maxFitness}`, () => {
     const pet = new Pet("fido");
-    pet.fitness = 8;
+    pet.fitness = maxFitness - 2;
     pet.walk();
-
-    expect(pet.fitness).toEqual(10);
+    expect(pet.fitness).toEqual(maxFitness);
   });
 
-  xit("throws an error if the pet is not alive", () => {
+  it("throws an error if the pet is not alive", () => {
     const pet = new Pet("Fido");
-    // As above, we dont know what the max age of a pet is. if it was in a variable, we could do this programatically by using pet.age =+ 5
-    pet.age = 35;
+    pet.age = maxAge + 1;
 
-    expect(() => pet.walk()).toThrow("Your pet is no longer alive :(");
+    expect(() => pet.walk()).toThrow(petDeadMessage);
   });
 });
 
@@ -113,27 +132,27 @@ describe("feed", () => {
     expect(pet.feed).toBeInstanceOf(Function);
   });
 
-  xit("decreases hunger by 3", () => {
+  it(`decreases hunger by ${feedReduction}`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 9;
+    pet.hunger = maxHunger - 1;
     pet.feed();
 
-    expect(pet.hunger).toEqual(6);
+    expect(pet.hunger).toEqual(maxHunger - 1 - feedReduction);
   });
 
-  xit("make sure hunger does not go below min hunger", () => {
+  it(`make sure hunger does not go below ${minHunger}`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 1;
+    pet.hunger = minHunger;
     pet.feed();
 
-    expect(pet.hunger).toEqual(0);
+    expect(pet.hunger).toEqual(minHunger);
   });
 
-  xit("throws an error if the pet is not alive", () => {
+  it("throws an error if the pet is not alive", () => {
     const pet = new Pet("Fido");
-    pet.hunger = 15;
+    pet.hunger = maxHunger + 1;
 
-    expect(() => pet.feed()).toThrow("Your pet is no longer alive :(");
+    expect(() => pet.feed()).toThrow(petDeadMessage);
   });
 });
 
@@ -144,70 +163,68 @@ describe("checkUp", () => {
     expect(pet.checkUp).toBeInstanceOf(Function);
   });
 
-  xit("returns I need a walk when fitness is 3 or less", () => {
+  it(`returns "${walkMessage}" when fitness is ${fitnessBreakPoint} or less`, () => {
     const pet = new Pet("fido");
-    pet.fitness = 3;
+    pet.fitness = fitnessBreakPoint;
 
-    // Could the messages for the checkup method be put in variables? If they were to change, the names of the tests would become redundant and they would all need re-writing to make them right.
-    // Also, if the limits for things like hunger and fitness were in variables they could be used in the names of the tests as well as in the tests themselves (think about template literals for this). Makes things much more futureproof and maintainable
-
-    expect(pet.checkUp()).toEqual("I need a walk");
-  });
-  xit("returns I am hungry when hunger is 5 or more", () => {
-    const pet = new Pet("fido");
-    pet.hunger = 5;
-
-    expect(pet.checkUp()).toEqual("I am hungry");
+    expect(pet.checkUp()).toEqual(walkMessage);
   });
 
-  xit("returns I am hungry AND I need a walk if fitness is 3 or less and hunger is 5 or more", () => {
+  it(`returns "${hungryMessage}" when hunger is ${hungerBreakPoint} or more`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 5;
-    pet.fitness = 3;
+    pet.hunger = hungerBreakPoint;
 
-    expect(pet.checkUp()).toEqual("I am hungry AND I need a walk");
+    expect(pet.checkUp()).toEqual(hungryMessage);
   });
 
-  xit("returns I feel great! if fitness is more than 3 and hunger is less than 5", () => {
+  it(`returns "${hungryAndWalkMessage}" if fitness is ${fitnessBreakPoint} or less and hunger is ${hungerBreakPoint} or more`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 4;
-    pet.fitness = 4;
+    pet.hunger = hungerBreakPoint;
+    pet.fitness = fitnessBreakPoint;
 
-    expect(pet.checkUp()).toEqual("I feel great!");
+    expect(pet.checkUp()).toEqual(hungryAndWalkMessage);
   });
 
-  xit("throws an error if the pet is not alive", () => {
+  it(`returns "${allHappyMessage}" if fitness is more than ${fitnessBreakPoint} and hunger is less than ${hungerBreakPoint}`, () => {
+    const pet = new Pet("fido");
+    pet.hunger = hungerBreakPoint - 1;
+    pet.fitness = fitnessBreakPoint + 1;
+
+    expect(pet.checkUp()).toEqual(allHappyMessage);
+  });
+
+  it("throws an error if the pet is not alive", () => {
     const pet = new Pet("Fido");
-    pet.fitness = 0;
+    pet.fitness = minFitness;
 
-    expect(() => pet.checkUp()).toThrow("Your pet is no longer alive :(");
+    expect(() => pet.checkUp()).toThrow(petDeadMessage);
   });
 });
 
 describe("isAlive", () => {
-  it("returns false if pet hunger is 10 or more, fitness 0 or less or age is 30 or more", () => {
+  it(`returns false if pet hunger is ${maxHunger} or more, fitness ${minFitness} or less or age is ${maxAge} or more`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 11;
-    pet.fitness = 0;
-    pet.age = 31;
+    pet.hunger = maxHunger + 1;
+    pet.fitness = minFitness;
+    pet.age = maxAge + 1;
 
     expect(pet.isAlive).toEqual(false);
   });
 
-  it("returns true if pet hunger is less than 10 and fitness is more than 0 and age is less than 30", () => {
+  it(`returns true if pet hunger is less than ${maxHunger} and fitness is more than ${minFitness} and age is less than ${maxAge}`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 9;
-    pet.fitness = 3;
-    pet.age = 6;
+    pet.hunger = maxHunger - 1;
+    pet.fitness = minFitness + 1;
+    pet.age = maxAge - 1;
 
     expect(pet.isAlive).toEqual(true);
   });
 
-  it("returns false if pet hunger is 10 or more, fitness 0 or less or age is 30 or more", () => {
+  it(`returns false if pet hunger is ${maxHunger} or more, fitness ${minFitness} or less or age is ${maxAge} or more`, () => {
     const pet = new Pet("fido");
-    pet.hunger = 4;
-    pet.fitness = 9;
-    pet.age = 30;
+    pet.hunger = minHunger + 1;
+    pet.fitness = maxFitness - 1;
+    pet.age = maxAge;
 
     expect(pet.isAlive).toEqual(false);
   });
--- src/pet.js
+++ src/pet.js
@@ -1,154 +1,163 @@
-// (function exportPet() {
-  const minFitness = 0;
-  const maxFitness = 10;
-  const minHunger = 0;
-  const maxHunger = 10;
-  const maxAge = 30;
-  const fitnessBreakPoint = 3;
-  const hungerBreakPoint = 5;
+const minFitness = 0;
+const maxFitness = 10;
+const minHunger = 0;
+const maxHunger = 10;
+const maxAge = 30;
+const fitnessBreakPoint = 3;
+const hungerBreakPoint = 5;
+const ageIncrement = 1;
+const hungerIncrement = 5;
+const fitnessReduction = 3;
+const feedReduction = 3;
+const exerciseIncrement = 4;
+const petDeadMessage = "Your pet is no longer alive :(";
+const hungryAndWalkMessage = "I am hungry AND I need a walk";
+const walkMessage = "I need a walk";
+const hungryMessage = "I am hungry";
+const allHappyMessage = "I feel great!";
+const statusCheck = typeof module !== "undefined" && module.exports;
 
-  function chooseAvatar(petType) {
-    createPet();
-    const setAvatarPicture = document.getElementById("petAlive");
-    document.getElementById("dinosaurAvatar").style.display = "none";
-    document.getElementById("unicornAvatar").style.display = "none";
-    document.getElementById("dogAvatar").style.display = "none";
-    if (petType === "dog") {
-      setAvatarPicture.src = "./images/virtualPet.jpg";
-      document.body.style.backgroundColor = "#01EDFA";
-    } else if (petType === "dinosaur") {
-      setAvatarPicture.src = "./images/dinosaur.jpg";
-      document.body.style.backgroundColor = "#DC9AFE";
-    } else if (petType === "unicorn") {
-      setAvatarPicture.src = "./images/unicornIceLolly.jpg";
-      document.body.style.backgroundColor = "#37DBFF";
+function chooseAvatar(petType) {
+  createPet();
+  const setAvatarPicture = document.getElementById("petAlive");
+  document.getElementById("dinosaurAvatar").style.display = "none";
+  document.getElementById("unicornAvatar").style.display = "none";
+  document.getElementById("dogAvatar").style.display = "none";
+  if (petType === "dog") {
+    setAvatarPicture.src = "./images/virtualPet.jpg";
+    document.body.style.backgroundColor = "#01EDFA";
+  } else if (petType === "dinosaur") {
+    setAvatarPicture.src = "./images/dinosaur.jpg";
+    document.body.style.backgroundColor = "#DC9AFE";
+  } else if (petType === "unicorn") {
+    setAvatarPicture.src = "./images/unicornIceLolly.jpg";
+    document.body.style.backgroundColor = "#37DBFF";
+  }
+}
+
+function createPet() {
+  let name;
+  const pet = new Pet((name = prompt("Name your pet", "name")));
+  const petNameMessage = document.getElementById("createPet");
+  window.pet = pet;
+  window.pet.checkUp();
+  petNameMessage.style.visibility = "visible";
+  petNameMessage.innerHTML = `Hello, my name is ${pet.name}!`;
+  document.getElementById("growUp").innerHTML = `${pet.age}`;
+}
+
+function pizzaAnimation() {
+  let id = null;
+  const elem = document.getElementById("pizzaSlice");
+  elem.style.visibility = "visible";
+  let posX = -370;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (posX === -100) {
+      elem.style.visibility = "hidden";
+      clearInterval(id);
+    } else {
+      posX++;
+      elem.style.left = posX + "px";
     }
   }
+}
 
-  function createPet() {
-    let name;
-    const pet = new Pet((name = prompt("Name your pet", "name")));
-    const petNameMessage = document.getElementById("createPet");
-    window.pet = pet;
-    window.pet.checkUp();
-    petNameMessage.style.visibility = "visible";
-    petNameMessage.innerHTML = `Hello, my name is ${pet.name}!`;
-    document.getElementById("growUp").innerHTML = `${pet.age}`;
-  }
-
-  function pizzaAnimation() {
-    let id = null;
-    const elem = document.getElementById("pizzaSlice");
-    elem.style.visibility = "visible";
-    let posX = -370;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (posX === -100) {
-        elem.style.visibility = "hidden";
-        clearInterval(id);
-      } else {
-        posX++;
-        elem.style.left = posX + "px";
-      }
+function exerciseAnimation() {
+  let id = null;
+  const elem = document.getElementById("petAlive");
+  let posY = 0;
+  let direction = "down";
+  let counter = 0;
+  clearInterval(id);
+  id = setInterval(frame);
+  function frame() {
+    if (counter === 11) {
+      clearInterval(id);
     }
-  }
-
-  function exerciseAnimation() {
-    let id = null;
-    const elem = document.getElementById("petAlive");
-    let posY = 0;
-    let direction = "down";
-    let counter = 0;
-    clearInterval(id);
-    id = setInterval(frame);
-    function frame() {
-      if (counter === 11) {
-        clearInterval(id);
-      }
-      if (posY === 50) {
-        direction = "up";
-        counter++;
-      }
-      if (posY === 0) {
-        direction = "down";
-        counter++;
-      }
-      if (direction === "down") {
-        posY++;
-      } else if (direction === "up") {
-        posY--;
-      }
-      elem.style.top = posY + "px";
+    if (posY === 50) {
+      direction = "up";
+      counter++;
     }
-  }
-
-  function Pet(name) {
-    this.name = name;
-    this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
-    this.children = [];
-  }
-
-  Pet.prototype = {
-    get isAlive() {
-      return (
-        this.age < maxAge &&
-        this.hunger < maxHunger &&
-        this.fitness > minFitness
-      );
-    },
-  };
-
-  Pet.prototype.growUp = function () {
-    if (!this.isAlive) {
-      throw new Error("Your pet is no longer alive :(");
+    if (posY === 0) {
+      direction = "down";
+      counter++;
     }
-    this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
+    if (direction === "down") {
+      posY++;
+    } else if (direction === "up") {
+      posY--;
+    }
+    elem.style.top = posY + "px";
+  }
+}
+
+function Pet(name) {
+  this.name = name;
+  this.age = 0;
+  this.hunger = minHunger;
+  this.fitness = maxFitness;
+  this.children = [];
+}
+
+Pet.prototype = {
+  get isAlive() {
+    return (
+      this.age < maxAge && this.hunger < maxHunger && this.fitness > minFitness
+    );
+  },
+};
+
+Pet.prototype.growUp = function () {
+  if (!this.isAlive) {
+    throw new Error(petDeadMessage);
+  }
+  this.age += ageIncrement;
+  this.hunger += hungerIncrement;
+  this.fitness -= fitnessReduction;
+  if (!statusCheck) {
     document.getElementById(
       "petMessage"
     ).innerHTML = `I am now ${this.age} years old.`;
     document.getElementById("growUp").innerHTML = `${this.age}`;
-  };
+  }
+};
 
-  Pet.prototype.walk = function () {
+Pet.prototype.walk = function () {
+  if (!this.isAlive) {
+    throw new Error(petDeadMessage);
+  }
+  if (!statusCheck) {
     const newPetMessage = document.getElementById("petMessage");
-    if (!this.isAlive) {
-      throw new Error("Your pet is no longer alive :(");
-    }
     if (this.fitness === maxFitness) {
       newPetMessage.innerHTML = "I don't need exercising.";
     } else {
       exerciseAnimation();
       newPetMessage.innerHTML = `Thank you for exercising me.`;
     }
-    this.fitness += 4;
-    if (this.fitness > maxFitness) {
-      this.fitness = maxFitness;
-    }
-  };
+  }
+  this.fitness = Math.min((this.fitness += exerciseIncrement), maxFitness);
+};
 
-  Pet.prototype.feed = function () {
+Pet.prototype.feed = function () {
+  if (!this.isAlive) {
+    throw new Error(petDeadMessage);
+  }
+  if (!statusCheck) {
     const newPetMessage = document.getElementById("petMessage");
-    if (!this.isAlive) {
-      throw new Error("Your pet is no longer alive :(");
-    }
     if (this.hunger === minHunger) {
       newPetMessage.innerHTML = "I don't need feeding.";
     } else {
       pizzaAnimation();
       newPetMessage.innerHTML = `Thank you for feeding me.`;
     }
-    this.hunger -= 3;
-    if (this.hunger < minHunger) {
-      this.hunger = minHunger;
-    }
-  };
+  }
+  this.hunger = Math.max((this.hunger -= feedReduction), minHunger);
+};
 
-  Pet.prototype.checkUp = function () {
+Pet.prototype.checkUp = function () {
+  if (!statusCheck) {
     const hungerStatusPicture = document.getElementById("hunger");
     const fitnessStatusPicture = document.getElementById("fitness");
     hungerStatusPicture.style.backgroundImage = "none";
@@ -158,7 +167,7 @@
       document.getElementById("petAlive").src = "./images/grimReaper.jpg";
       document.getElementById("petMessage").innerHTML =
         "Oh dear, I am no longer alive!";
-      throw new Error("Your pet is no longer alive :(");
+      throw new Error(petDeadMessage);
     }
     if (this.fitness <= fitnessBreakPoint && this.hunger >= hungerBreakPoint) {
       hungerStatusPicture.style.backgroundImage =
@@ -176,17 +185,48 @@
         "url('../images/petBowl.jpg')";
       hungerStatusPicture.style.backgroundSize = "cover";
     }
-  };
+  }
 
-  Pet.prototype.haveBaby = function (babyName) {
-    const child = new Pet(babyName);
-    this.children.push(child);
-  };
+  if (!this.isAlive) {
+    throw new Error(petDeadMessage);
+  }
+  if (this.fitness <= fitnessBreakPoint && this.hunger >= hungerBreakPoint) {
+    return hungryAndWalkMessage;
+  }
+  if (this.fitness <= fitnessBreakPoint) {
+    return walkMessage;
+  }
+  if (this.hunger >= hungerBreakPoint) {
+    return hungryMessage;
+  } else {
+    return allHappyMessage;
+  }
+};
 
-//   if (typeof module !== "undefined" && module.exports) {
-//     module.exports = Pet;
-//   } else {
-//     window.Pet = Pet;
-//   }
-//   module.exports = Pet;
-// })();
+Pet.prototype.haveBaby = function (babyName) {
+  const child = new Pet(babyName);
+  this.children.push(child);
+};
+
+if (statusCheck) {
+  module.exports = {
+    minFitness,
+    maxFitness,
+    minHunger,
+    maxHunger,
+    maxAge,
+    fitnessBreakPoint,
+    hungerBreakPoint,
+    ageIncrement,
+    hungerIncrement,
+    fitnessReduction,
+    feedReduction,
+    exerciseIncrement,
+    petDeadMessage,
+    hungryAndWalkMessage,
+    walkMessage,
+    hungryMessage,
+    allHappyMessage,
+    Pet,
+  };
+};
